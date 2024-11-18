f809e0af7583b01bc79554f3d40b936ca607055d
Mina-Shah <shahmina814@gmail.com>
2024-11-18 16:57:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+import FeatureSection from "./components/FeatureSection";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default App;
